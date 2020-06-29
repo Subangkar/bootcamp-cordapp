e876518249de938bc5ad6c2d917d3032bba5081b
Subangkar <subangkar.karmaker@gmail.com>
2020-06-29 20:00:37 +0600

    Token soln

--- src/main/java/bootcamp/TokenState.java
+++ src/main/java/bootcamp/TokenState.java
@@ -11,7 +11,38 @@ import java.util.List;
 
 /* Our state, defining a shared fact on the ledger.
  * See src/main/java/examples/ArtState.java for an example. */
-//@BelongsToContract(TokenContract.class)
-public class TokenState {
+@BelongsToContract(TokenContract.class)
+public class TokenState implements ContractState {
 
+	private Party issuer;
+	private Party owner;
+	private int amount;
+
+	public Party getIssuer() {
+		return issuer;
+	}
+
+	public Party getOwner() {
+		return owner;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public TokenState(Party issuer, Party owner, int amount) {
+		this.issuer = issuer;
+		this.owner = owner;
+		this.amount = amount;
+	}
+
+	@NotNull
+	@Override
+	public List<AbstractParty> getParticipants() {
+
+		List<AbstractParty> list = new ArrayList();
+		list.add(issuer);
+		list.add(owner);
+		return list;
+	}
 }
